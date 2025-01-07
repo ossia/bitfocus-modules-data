1821231d34f350986685a75fcad9787ade3dd757
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-01-06 22:08:07 -0500

    v1.0.1

--- create-release.sh
+++ create-release.sh
@@ -6,17 +6,21 @@ mkdir -p bitfocus-modules-data
     cd bitfocus-modules-data
     if [[ ! -d companion-bundled-modules ]]; then
         git clone --depth=1 https://github.com/ossia/companion-bundled-modules
-        rm -rf companion-bundled-modules/.git
     fi
+    rm -rf companion-bundled-modules/.git
 )
 
 for NODE_ARCH in linux-arm64.tar.xz linux-x64.tar.xz darwin-x64.tar.gz darwin-arm64.tar.gz win-x64.zip; do
     (
         cd bitfocus-modules-data
+        rm -rf node-runtime
         for NODE_VERSION in 18.20.5 22.12.0; do
-            mkdir -p node-runtime/$NODE_VERSION
             curl -L https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-$NODE_ARCH -o $NODE_VERSION-$NODE_ARCH
-            unar -q $NODE_VERSION-$NODE_ARCH -o node-runtime/
+            unar -q -D $NODE_VERSION-$NODE_ARCH -o node-runtime/
+            mv node-runtime/node-v* node-runtime/$NODE_VERSION
+            rm -rf node-runtime/$NODE_VERSION/include
+            rm -rf node-runtime/$NODE_VERSION/lib
+            rm -rf node-runtime/$NODE_VERSION/share
             rm $NODE_VERSION-$NODE_ARCH
         done
         
